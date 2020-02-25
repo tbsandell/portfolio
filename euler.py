ad6db1f850a11fc3278c0f1bfdf77e8fc6718d12
tbsandell <tombsandell@gmail.com>
2020-02-24 20:42:29 -0600

    added type annotations

--- euler.py
+++ euler.py
@@ -1,22 +1,16 @@
-def product(nums):
+from typing import List, TypeVar, Dict
+
+number = TypeVar("number", int, float)
+intOrList = TypeVar("intOrList", int, float)
+
+def product(nums: List[number]) -> number:
 	prod = 1
 	for num in nums:
 		prod *= num
 	return prod
 
-def checkInt(*nums):
-	for num in nums:
-		try:
-			assert int(num) == num
-		except AssertionError:
-			print(str(num) + ' is not an integer\nResults may be incorrect')
-			return False
-	return True
-
-"""checkInt"""
-def sumRange(start, lessThan, divisibleBy = 1):
-	"""Returns the sum of all numbers. Integers only"""
-	checkInt(start, lessThan, divisibleBy)
+def sumRange(start: int, lessThan: int, divisibleBy: int = 1) -> number:
+	"""Returns the sum of all numbers x where start <= x < lessThan and x mod divisibleBy = 0"""
 	sum = 0
 	while start % divisibleBy != 0:
 		sum += start
@@ -25,10 +19,8 @@ def sumRange(start, lessThan, divisibleBy = 1):
 	lessThan = int((lessThan - 1) / divisibleBy)
 	return int(sum + divisibleBy * ((lessThan ** 2 + lessThan) - (start ** 2 + start)) / 2)
 
-def isPalindrome(val):
-	"""Returns whether or not val is a palindrome. Integers and iterables only"""
-	if type(val) == float:
-		return False
+def isPalindrome(val: intOrList) -> bool:
+	"""Returns whether or not val is a palindrome"""
 	if type(val) == int:
 		val = str(val)
 	for i in range(len(val)):
@@ -40,10 +32,8 @@ def primes(lessThan):
 	"""Returns all primes less than lessThan"""
 	pass
 
-"""checkInt"""
-def primeFactors(num):
-	"""Returns the prime factors of num. Integers only"""
-	checkInt(num)
+def primeFactors(num: int) -> List[int]:
+	"""Returns the prime factors of num"""
 	arr = []
 	length = len(arr)
 	while num != 1:
@@ -58,7 +48,7 @@ def primeFactors(num):
 			break
 	return sorted(arr)
 
-def primeFactorsDict(num):
+def primeFactorsDict(num: int) -> Dict[int, int]:
 	arr = primeFactors(num)
 	d = dict()
 	for i in arr:
@@ -68,7 +58,7 @@ def primeFactorsDict(num):
 			d.update({i: 1})
 	return d
 
-def fib(lessThan):
+def fib(lessThan: int) -> List[int]:
 	"""Returns all fibonacci numbers < lessThan"""
 	arr = [0, 1]
 	while arr[-1] < lessThan:
@@ -77,16 +67,15 @@ def fib(lessThan):
 		arr.pop()
 	return arr
 
-def nFib(n):
+def nFib(n: int) -> List[int]:
 	"""Returns all fibonacci numbers up to the nth"""
 	arr = [0, 1]
 	while len(arr) < n:
 		arr.append(arr[-1] + arr[-2])
 	return arr
 
-"""checkInt"""
-def lcm(*nums):
-	"""Returns the least common multiple of *nums. Integers only"""
+def lcm(*nums: int) -> int:
+	"""Returns the least common multiple of *nums"""
 	primeDicts = [primeFactorsDict(num) for num in nums]
 	finalPrimeDict = dict()
 	final = 1
@@ -102,16 +91,11 @@ def lcm(*nums):
 	return final
 	
 
-def removeDuplicates(arr):
+def removeDuplicates(arr: list) -> list:
 	"""Returns an array with all duplicates removed"""
-	tempArr = []
-	for i in range(len(arr)):
-		if not arr[i] in arr[i + 1: len(arr)]:
-			tempArr.append(arr[i])
-	return tempArr
+	return list(set(arr))
 
-"""removeDuplicates"""
-def removeOneMultiple(nums):
+def removeOneMultiple(nums: List[int]) -> List[int]:
 	nums = sorted(removeDuplicates(nums))
 	for i in range(len(nums) - 1):
 		for j in range(i + 1, len(nums)):
@@ -120,8 +104,7 @@ def removeOneMultiple(nums):
 				return nums
 	return nums
 
-"""removeOneMultiple"""
-def removeMultiples(nums):
+def removeMultiples(nums: List[int]) -> List[int]:
 	"""Returns an array with all multiples removed ([2,3,4,5,6] -> [2,3,5])"""
 	arr = []
 	while arr != nums:
@@ -129,10 +112,8 @@ def removeMultiples(nums):
 		nums = removeOneMultiple(nums)
 	return nums
 
-"""checkInt, lcm, sumRange, removeMultiples"""
-def euler1(start, lessThan, *divisibleBy):
-	"""Returns the sum of all integers 'i' where greaterThan < i < lessThan and i is divisible by all divisibleBy. Integers only"""
-	checkInt(start, lessThan, *divisibleBy)
+def euler1(start: int, lessThan: int, *divisibleBy: int) -> int:
+	"""Returns the sum of all integers 'i' where greaterThan < i < lessThan and i is divisible by all divisibleBy"""
 	if len(divisibleBy) == 0:
 		return sumRange(start, lessThan)
 	elif len(divisibleBy) == 1:
@@ -151,20 +132,18 @@ def euler1(start, lessThan, *divisibleBy):
 		sum -= euler1(start, lessThan, *arr)
 		return sum
 
-def euler2(lessThan, *divisibleBy):
+def euler2(lessThan: int, *divisibleBy: int) -> int:
 	"""Returns the sum of all fibonacci numbers < lessThan and divisible by all divisibleBy. Whole numbers only"""
-	checkInt(*divisibleBy)
 	arr = fib(lessThan)
 	for divBy in divisibleBy:
 		arr = filter(lambda a: a % divBy == 0, arr)
 	return sum(arr)
 
-def euler3(num):
-	"""Returns the largest prime factor of num. Whole numbers only"""
-	checkInt(num)
+def euler3(num: int) -> int:
+	"""Returns the largest prime factor of num"""
 	return(primeFactors(num)[-1])
 
-def euler4():
+def euler4() -> int:
 	"""Returns the largest palindrome made from the product of two 3 digit numbers"""
 	i, arr = 999, []
 	while i > 99:
@@ -176,19 +155,19 @@ def euler4():
 		i -= 1
 	return max(arr)
 
-def euler5(*nums) :
-	"""Returns the smallest number divisible by all whole numbers less than lessThan. Whole numbers only"""
+def euler5(*nums: int) -> int:
+	"""Returns the smallest number divisible by all whole numbers less than lessThan"""
 	return lcm(*nums)
 
-def euler6(n):
+def euler6(n: int) -> int:
 	"""Returns the difference between the sum of the sqaures of the first n whole numbers and the sqaure of the sum of the first n whole numbers"""
 	return sum(range(n)) ** 2 - sum([i ** 2 for i in range(n)])
 
-def euler7(n):
+def euler7(n: int) -> int:
 	"""Returns the nth prime"""
 	n
 
-def euler700():
+def euler700() -> int:
 	arr0, arr1, m = [1504170715041707, 8912517754604], [1, 3], 4503599627370517
 	while arr0[-1] != 0:
 		for i in range(1000):
@@ -201,4 +180,4 @@ def euler700():
 
 	
 if __name__ == '__main__':
-	print(euler700())
+	print(lcm(1,2,3,4,5,6,7,8))
